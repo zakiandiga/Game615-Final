301cf664405c598d493959b95e269b584e8ea6c0
Zak <rusty_clarinet@yahoo.com>
2020-04-27 03:33:59 -0400

    Movement Update
    add sprint + animation
    add stamina system
    fixing die animation

---
+++ Assets/_Scripts/UIScripts/staminaBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class staminaBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxStamina(float stamina)
+    {
+        slider.maxValue = stamina;
+        slider.value = stamina;
+    }
+
+    public void SetStamina(float stamina)
+    {
+        slider.value = stamina;
+    }
+}
